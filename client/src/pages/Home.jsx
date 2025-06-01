a9c86c7976010be8e1f33ac2217a6264bec99f48
Vishal Oshada <vishaloshada@Vishals-MacBook-Air.local>
2025-06-02 00:53:25 +0530

    Exclusive offers styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from  '../components/Hero'
 import FeturedDestination  from '../components/FeturedDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeturedDestination />
+      <ExclusiveOffers/>
     </>
   )
 }
